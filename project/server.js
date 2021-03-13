bbac1669dcba492a61b38fae56dc628661681782
ch-monish <50499828+ch-monish@users.noreply.github.com>
2021-03-13 15:50:23 +0530

    cookie-bug

--- project/server.js
+++ project/server.js
@@ -3,8 +3,11 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express()
 const mysql = require("mysql")
+const cookieParser = require('cookie-parser')
 require("dotenv").config()
 
+var session = require('express-session');
+var MySQLStore = require('express-mysql-session')(session);
 
 
 var mysqlConnection = mysql.createConnection({
@@ -16,7 +19,7 @@ var mysqlConnection = mysql.createConnection({
 });
 mysqlConnection.connect((err) => {
     if (!err) {
-        console.log("connected");
+        console.log("connected to database");
     }
     else {
         console.log("error while connecting to database")
@@ -28,7 +31,44 @@ mysqlConnection.connect((err) => {
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-app.post('/', (req, res) => {
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+
+
+var options = {
+    host: "localhost",
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database,
+};
+
+var sessionStore = new MySQLStore(options);
+
+app.use(session({
+    key: 'session_cookie_name',
+    secret: 'secret',
+    store: sessionStore,
+    resave: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 1 },
+    saveUninitialized: false
+}));
+
+
+
+app.post('/load', (req, res) => {
+    if (req.session.authenticated == true) {
+        req.session.message = "login successfull"
+        res.send(req.session)
+    }
+    else {
+        req.session.message = "session not started"
+        res.send(req.session)
+    }
+})
+
+app.post('/login', (req, res) => {
+
     console.log(req.body)
     username = req.body.username
     password = req.body.password
@@ -42,8 +82,18 @@ app.post('/', (req, res) => {
         else {
             console.log(result)
             if (result != "") {
+
+                req.session.authenticated = true
+                req.session.user = username
+                // console.log("authenticated =" + req.session.authenticated)
+                console.log("------------------------------------------------------------------------------")
+                console.log(req.sessionID)
+                var sessionid = req.sessionID
+                console.log(sessionStore)
                 console.log("login success\n---------------------------------------------------------------")
-                res.send({ "message": "login successful" })
+                req.session.authenticated = true
+                res.cookie("sessionID", sessionid)
+                res.send({ "message": "login successfull", })
             }
             else {
                 console.log("Incorrect credentials\n---------------------------------------------------------------")
